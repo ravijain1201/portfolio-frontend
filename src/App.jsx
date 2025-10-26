77a0809f8ee7587e46cb57f8e26da044fbd05a3e
ravijain1201 <ravijain1201@gmail.com>
2025-10-27 00:27:00 +0530

    adding docker file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/hello')
+    fetch(`${process.env.REACT_APP_API_URL}/api/data`)
       .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
